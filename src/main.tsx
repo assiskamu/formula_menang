5756d6e6fb87a84e4a9b831b113bab84c25ecf7c
assiskamu <assiskamu@gmail.com>
2026-02-10 15:33:04 +0800

    Fix tooltip interactions across desktop and mobile

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import TooltipProvider from "./components/TooltipProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+    <TooltipProvider>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <App />
+      </BrowserRouter>
+    </TooltipProvider>
   </React.StrictMode>
 );
